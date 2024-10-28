ed465305ee27b3afd1290dc202ef5e887bf60734
Baban <avhandy946@gmail.com>
2024-10-28 20:32:50 +0700

    modified:   app/(root)/page.tsx 	new file:   components/HeaderBox.tsx 	new file:   components/TotalBalanceBox.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox"
+import TotalBalanceBox from "@/components/TotalBalanceBox"
+
+ 
 
 const Home = () => {
+  const loggedIn = { firstName: 'Afandy'}
   return (
     <section className='home'>
       <div className='home-content'>
         <header className='home-header'>
-          Welcome, Afandy
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          ></TotalBalanceBox>
         </header>
       </div>
     </section>
---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,17 @@
+const TotalBalanceBox = ({ accounts = [], totalBanks, totalCurrentBalance}: TotlaBalanceBoxProps ) => {
+  return (
+    <section className="total-balance">
+        <div className="total-balance-chart">
+            {/* DoughnutChart */}
+        </div>
+
+        <div className="flex flex-col gap-6">
+           <h2 className="header-2">
+                {totalBanks} Bank Accounts
+            </h2> 
+        </div>
+    </section>
+  )
+}
+
+export default TotalBalanceBox

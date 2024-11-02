f903b6a4115991980a97217c6107681d23195d74
Baban <avhandy946@gmail.com>
2024-11-02 15:50:38 +0700

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,10 @@
 import HeaderBox from "@/components/HeaderBox"
+import RightSidebar from "@/components/RightSidebar"
 import TotalBalanceBox from "@/components/TotalBalanceBox"
 
- 
 
 const Home = () => {
-  const loggedIn = { firstName: 'Afandy'}
+  const loggedIn = { firstName: 'Afandy', lastName: 'Baban', email: 'avhandy946@gmail.com'}
   return (
     <section className='home'>
       <div className='home-content'>
@@ -20,8 +20,15 @@ const Home = () => {
             accounts={[]}
             totalBanks={1}
             totalCurrentBalance={1250.35}
-          ></TotalBalanceBox>
+          >
+          </TotalBalanceBox>
         </header>
+
+          <RightSidebar 
+            user={loggedIn}
+            transactions={[]}
+            banks={[{}, {}]}
+          />
       </div>
     </section>
   )
